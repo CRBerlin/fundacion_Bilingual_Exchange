77afe27d80a589ce541e1f4951b9697ec91694d3
CRBerlin <crdq17@hotmail.com>
2026-05-14 21:38:16 +0000

    navbar completo. Index en proceso.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,64 @@
+/* ======================================
+   COMPONENTES
+====================================== */
+
 function cargarComponente(id, archivo) {
     fetch(archivo)
         .then(response => response.text())
         .then(data => {
             document.getElementById(id).innerHTML = data;
+
+            if (id === 'navbar') {
+                initializeLanguageSystem();
+            }
         });
 }
 
 cargarComponente('navbar', 'components/navbar.html');
 cargarComponente('registro', 'components/registro.html');
 cargarComponente('footer', 'components/footer.html');
+
+/* ======================================
+   IDIOMAS
+====================================== */
+
+let currentLanguage = 'es';
+
+function translatePage() {
+    const currentLanguageElement = document.getElementById('currentLanguage');
+
+    if (currentLanguageElement) {
+        currentLanguageElement.textContent =
+            translations[currentLanguage].language_short;
+    }
+
+    document.querySelectorAll('.translate')
+        .forEach(element => {
+
+            const key = element.dataset.key;
+
+            if (translations[currentLanguage][key]) {
+                element.textContent =
+                    translations[currentLanguage][key];
+            }
+        });
+}
+
+function changeLanguage(language) {
+    currentLanguage = language;
+    translatePage();
+}
+
+function initializeLanguageSystem() {
+    document.addEventListener('click', function (e) {
+
+        const button = e.target.closest('.change-language');
+
+        if (button) {
+            const selectedLanguage = button.dataset.language;
+            changeLanguage(selectedLanguage);
+        }
+    });
+
+    translatePage();
+}
---
+++ assets/js/languages.js
@@ -0,0 +1,20 @@
+const translations = {
+    es: {
+        nav_about: "¿Quiénes Somos?",
+        nav_program: "Programa para extranjeros",
+        nav_accommodation: "Hospedaje",
+        nav_volunteering: "Voluntariado",
+        nav_register: "Regístrate",
+        language_name: "Español",
+        language_short: "ES"
+    },
+    en: {
+        nav_about: "Who Are We?",
+        nav_program: "Program for Foreigners",
+        nav_accommodation: "Accommodation",
+        nav_volunteering: "Volunteering",
+        nav_register: "Register",
+        language_name: "English",
+        language_short: "EN"
+    }
+};
